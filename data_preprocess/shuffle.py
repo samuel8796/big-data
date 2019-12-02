9261fcd431a711776414ed6ee9fc5696b46cd5c0
samuel8796 <40731162+samuel8796@users.noreply.github.com>
2019-12-02 14:33:24 +0800

    Update shuffle.py

--- data_preprocess/shuffle.py
+++ data_preprocess/shuffle.py
@@ -1,5 +1,7 @@
-def shuffle(X,Y):
-    np.random.seed(10)
+import numpy as np
+
+def shuffle(X,Y,seed):
+    np.random.seed(seed)
     randomList = np.arange(X.shape[0])
     np.random.shuffle(randomList)
     return X[randomList], Y[randomList]
