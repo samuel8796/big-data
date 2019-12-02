6691ab6b836548ea741240b8172c567a38837247
samuel8796 <40731162+samuel8796@users.noreply.github.com>
2019-12-02 14:36:18 +0800

    Update dataIO.py

--- data_preprocess/dataIO.py
+++ data_preprocess/dataIO.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
-from config import config
-[2,3,26,33,34,36,39,40,49,50,51,52,53,54,60,61,65]
+from config import listofcolumn
+
 
 def load_row_dataset(path):
-    list_of_column = config
+    list_of_column = listofcolumn()
     p = Path(path)
     row_data = pd.read_excel(p)
     row_data = row_data.iloc[:,list_of_column]
