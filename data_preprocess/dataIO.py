9229c1e25ab9e54bf83acfaefc6c4b753dcaf8db
samuel8796 <40731162+samuel8796@users.noreply.github.com>
2019-12-02 14:32:32 +0800

    Update dataIO.py

--- data_preprocess/dataIO.py
+++ data_preprocess/dataIO.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
+from config import config
+[2,3,26,33,34,36,39,40,49,50,51,52,53,54,60,61,65]
 
 def load_row_dataset(path):
+    list_of_column = config
     p = Path(path)
-    csv_list = list(p.glob('**/*.xlsx'))
-    row_data_list = []
-    
-    for i in csv_list:
-        row_data = pd.read_excel(i)
-        #row_data = row_data[[' - MARKET VALUE',' - DIVIDEND YIELD',' - EARNINGS PER SHR',' - PRICE HIGH',' - PRICE LOW',' - OPENING PRICE',' - TURNOVER BY VALUE',' - TURNOVER BY VOLUME',' - BK VAL PS 12M FWD',' - EV 12M FWD',' - NET INCOME 12M FWD',' - PRETAX PRF 12M FWD',' - ROE 12M FWD',' - SALES 12M FWD',' - P/CSH FLOW RATIO',' - PRICE TO BOOK VAL',' - NUMBER OF SHARES']]
-        row_data_list.append(row_data)
-        
-    return row_data_list
+    row_data = pd.read_excel(p)
+    row_data = row_data.iloc[:,list_of_column]
+    return row_data
+
+
